840e46162e5d3b640a51f0939e2baf342fc82991
Amado Manalo Jr <amanalojr@gmail.com>
2021-06-03 02:29:19 -0400

    main bug

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import MenuDirectory from './MenuDirectoryComponent';
 import RecipeInfo from './RecipeInfoComponent';
-import { Text, View } from 'react-native';
-import { Card } from 'react-native-elements';
+import { View } from 'react-native';
 import { Recipes } from '../shared/recipes';
 
 class Main extends Component {
@@ -17,18 +16,17 @@ class Main extends Component {
     onRecipeSelect(recipeId) {
         this.setState({selectedrecipe: recipeId});
     }
-
+    
     render() {
         return (
         <View style={{flex: 1}}>
-        <MenuDirectory 
-            recipes={this.state.recipes}
-            onPress={recipeId => this.onRecipeSelect(recipeId)} />;
-        <RecipeInfo
-                    recipe={this.state.recipes.filter(
-                    recipe => recipe.id === this.state.selectedRecipe)[0]}
-                />;
-
+            <MenuDirectory 
+                recipes={this.state.recipes}
+                onPress={recipeId => this.onRecipeSelect(recipeId)} />;
+            <RecipeInfo
+                recipe={this.state.recipes.filter(
+                recipe => recipe.id === this.state.selectedRecipe)[0]}
+            />;
         </View>
         );
     }
