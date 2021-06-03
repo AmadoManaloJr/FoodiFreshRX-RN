d9e3436f060efc7b1b3e6d12a5768658a363f01e
Amado Manalo Jr <amanalojr@gmail.com>
2021-06-02 23:30:37 -0400

    created main and menudirectory

---
+++ components/MainComponent.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import MenuDirectory from './MenuDirectoryComponent';
+import { Recipes } from '../shared/Recipes';
+
+class Main extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            recipes : Recipes
+        };
+    }
+
+    render() {
+        return <MenuDirectory recipes={this.state.recipes} />;
+    }
+}
+
+export default Main;
